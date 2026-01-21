6962d34df4efa6fb34a848e4f645f9589b1dab4b
Abjura <107774277+Abjuraa@users.noreply.github.com>
2026-01-20 21:46:56 -0500

    refactor se modifico la api para que el paginador quede exclusivamente para el admin

--- src/main/java/com/onlyphones/onlyphones/controller/admin/AdminProductController.java
+++ src/main/java/com/onlyphones/onlyphones/controller/admin/AdminProductController.java
@@ -6,10 +6,13 @@ import com.onlyphones.onlyphones.entity.Product;
 import com.onlyphones.onlyphones.service.CloudinaryService;
 import com.onlyphones.onlyphones.service.ProductService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/admin")
@@ -20,6 +23,8 @@ public class AdminProductController extends ProductAbstractController {
     private final ObjectMapper objectMapper;
     private final CloudinaryService cloudinaryService;
     private final ProductService productService;
+    public static final String PAGE_DEFAULT = "0";
+    public static final String SIZE_DEFAULT = "10";
 
     public AdminProductController(ProductService productService, ObjectMapper objectMapper, CloudinaryService cloudinaryService, ProductService productService1) {
         super(productService);
@@ -28,6 +33,24 @@ public class AdminProductController extends ProductAbstractController {
         this.productService = productService1;
     }
 
+    // product/paginador?page=0&size=16
+    @GetMapping("/product/paginador")
+    public ResponseEntity<Map<String, Object>> pageProducts(
+            @RequestParam(defaultValue = PAGE_DEFAULT) int page,
+            @RequestParam(defaultValue = SIZE_DEFAULT) int size
+    ) {
+        int pageSize = Math.min(size, 24);
+        Page<Product> product = productService.pagerProducts(page, pageSize);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("content", product.getContent());
+        response.put("currentPage", product.getNumber());
+        response.put("totalItems", product.getTotalElements());
+        response.put("totalPages", product.getTotalPages());
+        response.put("pageSize", product.getSize());
+
+        return ResponseEntity.ok(response);
+    }
 
     @PostMapping("/createproduct")
     public ResponseEntity<Product> createProduct(
--- src/main/java/com/onlyphones/onlyphones/controller/client/ClientProductController.java
+++ src/main/java/com/onlyphones/onlyphones/controller/client/ClientProductController.java
@@ -16,8 +16,6 @@ import java.util.Map;
 
 public class ClientProductController extends ProductAbstractController {
 
-    public static final String PAGE_DEFAULT = "0";
-    public static final String SIZE_DEFAULT = "16";
     private final ProductService productService;
 
     public ClientProductController(ProductService productService, ProductService productService1) {
@@ -25,25 +23,6 @@ public class ClientProductController extends ProductAbstractController {
         this.productService = productService1;
     }
 
-
-    // product/paginador?page=0&size=16
-    @GetMapping("/product/paginador")
-    public ResponseEntity<Product> pageProducts(
-            @RequestParam(defaultValue = PAGE_DEFAULT) String page,
-            @RequestParam(defaultValue = SIZE_DEFAULT) String size
-    ) {
-        int minSize = Math.min(Integer.getInteger(size), 24);
-        Page<Product> product = productService.pagerProducts(Integer.getInteger(page), minSize);
-
-        Map<String, Object> response = new HashMap<>();
-        response.put("content", product.getContent());
-        response.put("currentPage", product.getNumber());
-        response.put("totalItems", product.getTotalElements());
-        response.put("totalPages", product.getTotalPages());
-        response.put("pageSize", product.getSize());
-
-        return ResponseEntity.ok((Product) response);
-    }
 
     @GetMapping("/product/latest")
     public ResponseEntity<List<Product>> getLatestProduct() {
